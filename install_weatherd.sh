d685e748fbb5aef05ef30e538e5ab5cd377548ef
kevhnmay94 <kevhnmay94@gmail.com>
2022-08-30 11:26:37 +0700

    refactor

--- install_weatherd.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-ABSPATH=$(cd "$(dirname "$0")"; pwd -P)
-cp $ABSPATH/weatherd /usr/local/bin
-cp $ABSPATH/com.apple.weatherd.plist /Library/LaunchDaemons
-chown root:wheel /usr/local/bin/weatherd /Library/LaunchDaemons/com.apple.weatherd.plist
-launchctl unload -w /Library/LaunchDaemons/com.apple.weatherd.plist
-launchctl load -w /Library/LaunchDaemons/com.apple.weatherd.plist
---
+++ install_weathersd.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+ABSPATH=$(cd "$(dirname "$0")"; pwd -P)
+cp $ABSPATH/weathersd /usr/local/bin
+cp $ABSPATH/com.apple.weathersd.plist /Library/LaunchDaemons
+chown root:wheel /usr/local/bin/weathersd /Library/LaunchDaemons/com.apple.weathersd.plist
+launchctl unload -w /Library/LaunchDaemons/com.apple.weathersd.plist
+launchctl load -w /Library/LaunchDaemons/com.apple.weathersd.plist
