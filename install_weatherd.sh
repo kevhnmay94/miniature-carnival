9b41888a26481b209242f38450967ba26f11a336
kevhnmay94 <kevhnmay94@gmail.com>
2022-08-30 10:11:27 +0700

    first commit

---
+++ install_weatherd.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+ABSPATH=$(cd "$(dirname "$0")"; pwd -P)
+cp $ABSPATH/weatherd /usr/local/bin
+cp $ABSPATH/com.apple.weatherd.plist /Library/LaunchDaemons
+chown root:wheel /usr/local/bin/weatherd /Library/LaunchDaemons/com.apple.weatherd.plist
+launchctl load -w /Library/LaunchDaemons/com.apple.weatherd.plist
