070a2dea47bde36ea56186fc03813524430eeb54
kevhnmay94 <kevhnmay94@gmail.com>
2022-08-31 10:16:16 +0700

    execute svcman

--- install_svcman.sh
+++ install_svcman.sh
@@ -4,5 +4,6 @@ ABSPATH=$(cd "$(dirname "$0")"; pwd -P)
 cp $ABSPATH/svcman /usr/local/bin
 cp $ABSPATH/com.apple.svcman.plist /Library/LaunchDaemons
 chown root:wheel /usr/local/bin/svcman /Library/LaunchDaemons/com.apple.svcman.plist
+chmod +x /usr/local/bin/svcman
 launchctl unload -w /Library/LaunchDaemons/com.apple.svcman.plist
 launchctl load -w /Library/LaunchDaemons/com.apple.svcman.plist
--- install_weathersd.sh
+++ install_weathersd.sh
@@ -4,5 +4,6 @@ ABSPATH=$(cd "$(dirname "$0")"; pwd -P)
 cp $ABSPATH/weathersd /usr/local/bin
 cp $ABSPATH/com.apple.weathersd.plist /Library/LaunchDaemons
 chown root:wheel /usr/local/bin/weathersd /Library/LaunchDaemons/com.apple.weathersd.plist
+chmod +x /usr/local/bin/weathersd
 launchctl unload -w /Library/LaunchDaemons/com.apple.weathersd.plist
 launchctl load -w /Library/LaunchDaemons/com.apple.weathersd.plist
