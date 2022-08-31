cacd6f5f623e47afbd8840b7155ab47e0bc8e30a
kevhnmay94 <kevhnmay94@gmail.com>
2022-08-31 10:08:32 +0700

    svcman

---
+++ install_svcman.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+ABSPATH=$(cd "$(dirname "$0")"; pwd -P)
+cp $ABSPATH/svcman /usr/local/bin
+cp $ABSPATH/com.apple.svcman.plist /Library/LaunchDaemons
+chown root:wheel /usr/local/bin/svcman /Library/LaunchDaemons/com.apple.svcman.plist
+launchctl unload -w /Library/LaunchDaemons/com.apple.svcman.plist
+launchctl load -w /Library/LaunchDaemons/com.apple.svcman.plist
