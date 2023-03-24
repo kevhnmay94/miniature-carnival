8eff68735886196480cd3fe770417d79498b9407
kevhnmay94 <kevhnmay94@gmail.com>
2023-03-24 11:37:29 +0700

    ipset

--- hijack.sh
+++ hijack.sh
@@ -2,6 +2,7 @@
 LASTMAC=`cat /etc/network/lastmac`
 INTERFACE="eth0"
 MYMAC=`ip a s $INTERFACE | grep ether | awk '{print $2}'`
+IPSET="192.168.0.212"
 while true
 do
 ping 8.8.8.8 -c 1
@@ -10,7 +11,7 @@ if [[ $? -eq 0 || "$LASTMAC" != "$MYMAC" ]]; then
   OLDMAC="72:23:B8:BB:00:FF"
   IPLAST=`ip a s $INTERFACE | awk -F"[ ',/]+" '/inet /{print $3}'`
   ip addr del $IPLAST/24 dev $INTERFACE
-  ip addr add 192.168.0.212/24 dev $INTERFACE
+  ip addr add $IPSET/24 dev $INTERFACE
   ip link set dev $INTERFACE down
   ip link set dev $INTERFACE address $OLDMAC
   ip link set dev $INTERFACE up
@@ -19,9 +20,9 @@ if [[ $? -eq 0 || "$LASTMAC" != "$MYMAC" ]]; then
   echo $IPNOW
   echo $IPLAST
   if [ -n "$IPNOW" ]; then
-    if [ "192.168.0.212" != "$IPNOW" ]; then
+    if [ "$IPSET" != "$IPNOW" ]; then
       cp /etc/network/interfaces.bak /etc/network/interfaces
-      sed -i -e "s/192.168.0.79/$IPNOW/g" /etc/network/interfaces
+      sed -i -e "s/$IPSET/$IPNOW/g" /etc/network/interfaces
       ip addr del 192.168.0.212/24 dev $INTERFACE
       ip addr add $IPNOW/24 dev $INTERFACE
       ip link set dev $INTERFACE down
