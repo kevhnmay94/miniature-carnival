11c59d671f5396c60b02f6730f8b0fa13a38ae66
kevhnmay94 <kevhnmay94@gmail.com>
2023-04-28 17:03:35 +0700

    update backup wifi

--- backup_wifi.sh
+++ backup_wifi.sh
@@ -1,21 +1,20 @@
 #! /bin/bash
+# WifiBridge must be active first
 while true
 do
  nmcli d wifi list --rescan yes | grep -e "E8:FC:AF:F5:D7:55"
  if [[ $? != 0 ]]; then
   echo "Wifi down, switching to backup"
-  nmcli c show --active | grep WifiBridge
+  nmcli c show --active | grep Hotstop
   if [[ $? != 0 ]]; then
-   nmcli c down "Wired connection 1"
-   nmcli c up WifiBridge
+   nmcli c up Hotstop
   fi
  else
   echo "Wifi up, turn off backup"
-  nmcli c show --active | grep "Wired connection 1"
-  if [[ $? != 0 ]]; then
-   nmcli c down WifiBridge
-   nmcli c up "Wired connection 1"
+  nmcli c show --active | grep Hotstop
+  if [[ $? == 0 ]]; then
+   nmcli c down Hotstop
   fi
  fi
- sleep 30
+ sleep 20
 done
